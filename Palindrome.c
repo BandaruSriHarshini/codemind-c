b74a6ce996f71825a8b377a8a0fe091fceead307
Bandaru Sri Harshini <97589283+BandaruSriHarshini@users.noreply.github.com>
2022-05-19 20:46:52 +0530

    #include<stdio.h> int main() { int n, t, sum = 0, remainder ; scanf ("%d", &n); t = n; while (n>0) { remainder = n % 10; sum = sum *10 + remainder ; n = n/ 10; } if(t== sum ) printf ("True"); else printf ("False"); return 0; }

---
+++ Palindrome.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n, t, sum = 0, remainder ;
+scanf ("%d", &n);
+t = n;
+while (n>0)
+{
+remainder = n % 10;
+sum = sum *10 + remainder ;
+n = n/ 10;
+}
+if(t== sum )
+printf ("True");
+else
+printf ("False");
+return 0;
+}
