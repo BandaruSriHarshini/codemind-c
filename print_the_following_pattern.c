41cf622326baecc34d781a2d6798074a191f0828
Bandaru Sri Harshini <97589283+BandaruSriHarshini@users.noreply.github.com>
2022-06-27 22:24:01 +0530

    #include<stdio.h> int main() {     int i,j,a;     scanf("%d",&a);     for(i=a;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%d",j);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,N;
-    scanf("%d",&N);
-    for(i=1;i<=N;i++)
+    int i,j,a;
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
     {
         for(j=1;j<=i;j++)
         {
